6c4c71f92f960e27a7519f795effc4bdd8bd3448
Munyaradzi Magura <munyaradziMagura@outlook.com>
2022-06-03 16:50:28 +0930

    add file loader adapter

--- PatientSystem/FileLoaderAdapter.cpp
+++ PatientSystem/FileLoaderAdapter.cpp
@@ -1,6 +1,4 @@
 #include "FileLoaderAdapter.h"
-#include <iostream>
-using namespace std;
 
 void FileLoaderAdapter::initialiseConnection()
 {
@@ -8,12 +6,8 @@ void FileLoaderAdapter::initialiseConnection()
 
 void FileLoaderAdapter::loadPatients(std::vector<Patient*>& patientIn)
 {
-	cout << "FILE WORKING" << endl;
-
-	auto textFile = _PatientFileLoader->loadPatientFile("patients.txt");
-	
-	patientIn.insert(patientIn.end(), textFile.begin(), textFile.end());
-
+	auto loadTextFile = _PatientFileLoader->loadPatientFile("patients.txt");
+	patientIn.insert(patientIn.end(), loadTextFile.begin(), loadTextFile.end());
 }
 
 void FileLoaderAdapter::closeConnection()
--- PatientSystem/FileLoaderAdapter.h
+++ PatientSystem/FileLoaderAdapter.h
@@ -1,20 +1,21 @@
 #pragma once
 #include "AbstractPatientDatabaseLoader.h"
 #include "PatientFileLoader.h"
-class FileLoaderAdapter :
-    public AbstractPatientDatabaseLoader
+
+class FileLoaderAdapter : public AbstractPatientDatabaseLoader
 {
-private:
-    PatientFileLoader* _PatientFileLoader;
+
 public:
-    FileLoaderAdapter(PatientFileLoader* PatientFileLoader) {
-        _PatientFileLoader = PatientFileLoader;
-    }
-	// initialise the database connection
-    virtual void initialiseConnection() override;
 
-    virtual void loadPatients(std::vector<Patient*>& patientIn) override;
+	virtual void initialiseConnection() override;
 
-    virtual void closeConnection() override;
+	virtual void loadPatients(std::vector<Patient*>& patientIn) override;
+
+	virtual void closeConnection() override;
+
+private:
+
+	PatientFileLoader* _PatientFileLoader;
+
 };
 
