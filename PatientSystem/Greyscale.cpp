0d1c6b76cb4656fc1160a9726f1d2ad01c51c0f2
Munyaradzi Magura <munyaradziMagura@outlook.com>
2022-06-03 17:20:16 +0930

    print not needed

--- PatientSystem/Greyscale.cpp
+++ PatientSystem/Greyscale.cpp
@@ -3,11 +3,9 @@
 void Greyscale::algorithmInterface(float bodyTemperature, int bloodPressure, int heartRate, int respitoryRate, string lastname, string firstname, string id, int age, Patient* p)
 {
     if (age < 12 && heartRate > 120 ) { 
-        std::cout << "Patient:" << id << " has an alert level: " << "RED" << std::endl;
         p->setAlertLevel(AlertLevel::Red);
     }
     if (age >= 12 && heartRate > 100) { 
-        std::cout << "Patient:" << id << " has an alert level: " << "RED" << std::endl;
         p->setAlertLevel(AlertLevel::Red);
     }
 }
