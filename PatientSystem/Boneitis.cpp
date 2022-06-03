6af96ebb91734a7a1412db5bc88c14da1ed5c100
Munyaradzi Magura <munyaradziMagura@outlook.com>
2022-06-03 16:29:48 +0930

    set alertLevels

--- PatientSystem/Boneitis.cpp
+++ PatientSystem/Boneitis.cpp
@@ -2,7 +2,16 @@
 
 void Boneitis::algorithmInterface(float bodyTemperature, int bloodPressure, int heartRate, int respitoryRate, string lastname, string firstname, string id, int age, Patient* p)
 {
-    if (respitoryRate > 20 && respitoryRate < 30) { std::cout << "Patient:"  <<id << " has an alert level: " << "YELLOW" << std::endl; }
-    if (respitoryRate > 30 && respitoryRate < 40) { std::cout << "Patient:" << id << " has an alert level: " << "ORANGE" << std::endl; }
-    if (respitoryRate > 40) { std::cout << "Patient: " << id << " has an alert level: " << "RED" << std::endl; }
+    if (respitoryRate > 20 && respitoryRate < 30) { 
+        std::cout << "Patient:"  <<id << " has an alert level: " << "YELLOW" << std::endl;
+        p->setAlertLevel(AlertLevel::Yellow);
+    }
+    if (respitoryRate > 30 && respitoryRate < 40) { 
+        std::cout << "Patient:" << id << " has an alert level: " << "ORANGE" << std::endl;
+        p->setAlertLevel(AlertLevel::Orange);
+    }
+    if (respitoryRate > 40) { 
+        std::cout << "Patient: " << id << " has an alert level: " << "RED" << std::endl;
+        p->setAlertLevel(AlertLevel::Red);
+    }
 }
--- PatientSystem/SimianFlu.cpp
+++ PatientSystem/SimianFlu.cpp
@@ -2,7 +2,16 @@
 
 void SimianFlu::algorithmInterface(float bodyTemperature, int bloodPressure, int heartRate, int respitoryRate, string lastname, string firstname, string id, int age, Patient* p)
 {
-    if (bloodPressure > 140) { std::cout << "Patient:" << id << " has an alert level: " << "RED" << std::endl; }
-    if (bloodPressure > 130) { std::cout << "Patient:" << id << " has an alert level: " << "ORANGE" << std::endl; }
-    if (bloodPressure > 110) { std::cout << "Patient:" << id << " has an alert level: " << "YELLOW" << std::endl; }
+    if (bloodPressure > 140) { 
+        std::cout << "Patient:" << id << " has an alert level: " << "RED" << std::endl; 
+        p->setAlertLevel(AlertLevel::Red);
+    }
+    if (bloodPressure > 130) { 
+        std::cout << "Patient:" << id << " has an alert level: " << "ORANGE" << std::endl; 
+        p->setAlertLevel(AlertLevel::Orange);
+    }
+    if (bloodPressure > 110) { 
+        std::cout << "Patient:" << id << " has an alert level: " << "YELLOW" << std::endl;
+        p->setAlertLevel(AlertLevel::Yellow);
+    }
 }
